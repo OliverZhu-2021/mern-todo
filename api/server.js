89021c3e36b3ff2381c1f52351c6bf72637aa20e
Hao Zhu <zhuhao_0227@sina.com>
2025-03-29 12:24:52 +1100

    Fix nonstandard asynchronous syntax

--- api/server.js
+++ api/server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect('mongodb://127.0.0.1:27017/react-todo', {
+mongoose.connect('mongodb://127.0.0.1:27017/todolist', {
 	useNewUrlParser: true, 
 	useUnifiedTopology: true 
 }).then(() => console.log("Connected to MongoDB")).catch(console.error);
